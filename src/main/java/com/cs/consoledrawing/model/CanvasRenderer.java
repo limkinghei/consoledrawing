a2cc346b7c22a540dd80ef36acdace26c36ba204
limkinghei <54285209+limkinghei@users.noreply.github.com>
2019-08-26 23:11:07 -0400

    Update CanvasRenderer.java

--- src/main/java/com/cs/consoledrawing/model/CanvasRenderer.java
+++ src/main/java/com/cs/consoledrawing/model/CanvasRenderer.java
@@ -5,27 +5,22 @@ public class CanvasRenderer {
     private static final char HORIZONTAL_EDGE_CHAR = '-';
     private static final char VERTICAL_EDGE_CHAR   = '|';
 
-    public static void render(Canvas canvas){
+    public void render(Canvas canvas, PrintStream printStream){
         int height = canvas.getHeight();
         int width = canvas.getWidth();
 
         for (int row = 0; row <= height + 1; row++){
-            StringBuilder toPrint = new StringBuilder();
 
             for (int col = 0; col <= width + 1; col++){
 
                 if (row == 0 || row == height + 1){
-                    toPrint.append(HORIZONTAL_EDGE_CHAR);
-                }
-                else if (col == 0 || col == width + 1){
-                    toPrint.append(VERTICAL_EDGE_CHAR);
-                }
-                else{
-                    toPrint.append(canvas.getPixel(row, col));
+                    printStream.print(HORIZONTAL_EDGE_CHAR);
+                } else if (col == 0 || col == width + 1){
+                    printStream.print(VERTICAL_EDGE_CHAR);
+                } else{
+                    printStream.print(canvas.getPixel(row, col));
                 }
             }
-
-            System.out.println(toPrint.toString());
         }
     }
 }
